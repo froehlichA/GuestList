06b25fc40bc5a68ec787a87f1315c9816db8e5ea
Fröhlich A <froehlich.alexander@protonmail.com>
2018-04-05 13:05:46 +0200

    Improve Service connection over static url

--- frontend/src/app/+gaesteliste/gaesteliste.service.ts
+++ frontend/src/app/+gaesteliste/gaesteliste.service.ts
@@ -4,12 +4,14 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class GaestelisteService {
+  backendUrl: string = "http://localhost:8080";
+  backendData = endpoint => `${this.backendUrl}/${endpoint}`;
 
   guests: Array<Guest> = [];
 
   constructor(private _httpClient: HttpClient) { }
 
   loadData(): any {
-    return this._httpClient.get('http://localhost:8080/guests');
+    return this._httpClient.get(this.backendData('guests'));
   }
 }
